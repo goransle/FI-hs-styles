2597899fc21d80eba28912c9a843165e2c2dcf38
Marita <marita.vindedal@highsoft.com>
2020-05-29 08:15:36 +0200

    Added polar chart demo

---
+++ polar-chart/hs.js
@@ -0,0 +1,68 @@
+import Highcharts from 'https://code.highcharts.com/es-modules/masters/highcharts.src.js';
+import 'https://code.highcharts.com/es-modules/masters/highcharts-more.src.js';
+import 'https://code.highcharts.com/es-modules/masters/modules/data.src.js';
+import 'https://code.highcharts.com/es-modules/masters/modules/exporting.src.js';
+
+
+// Netto nedlastingar per måned
+Highcharts.chart("container", {
+    chart: {
+        type: 'column',
+        inverted: true,
+        polar: true,
+        styledMode: true
+    },
+    title: {
+        text: 'Antall nedlastingar per månad'
+    },
+    subtitle: {
+    		text: 'Juni 2020'
+    },
+    tooltip: {
+        outside: true
+    },
+    pane: {
+        size: '85%',
+        innerSize: '20%',
+        endAngle: 270
+    },
+    xAxis: {
+    		
+        tickInterval: 1,
+        labels: {
+            align: 'right',
+            useHTML: true,
+            allowOverlap: true,
+            step: 1,
+            y: 3,
+            style: {
+                fontSize: '13px'
+            }
+        },
+        lineWidth: 0,
+    },
+    yAxis: {
+        crosshair: {
+            enabled: true,
+            color: '#333'
+        },
+        lineWidth: 0,
+        tickInterval: 200,
+        reversedStacks: false,
+        endOnTick: true,
+        showLastLabel: true
+    },
+    plotOptions: {
+        column: {
+            stacking: 'normal',
+            borderWidth: 0,
+            pointPadding: 0,
+            groupPadding: 0.15
+        },
+    },
+  data: {
+    googleSpreadsheetKey: "1da82Nx3vYm14msH7oYtdYkrXoSSmsU84xlf8EMIofNg",
+    googleSpreadsheetWorksheet: 7, // fane nr 7
+ 
+  }
+});
